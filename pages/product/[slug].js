76826bcd46c2e7710e6ed23a195c73cfe5e91ad9
Ayan Das Karmakar <coderayan1411@gmail.com>
2023-06-29 23:01:21 +0530

    Changed color of the heart button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -97,7 +97,7 @@ const Slug = () => {
                             <span className="title-font font-medium text-2xl text-gray-900">&#8377;499</span>
                             <button className="flex items-center ml-auto xl:ml-20 text-white bg-pink-500 border-0 py-1.5 md:py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded text-sm md:text-base">Buy Now</button>
                             <button className="flex items-center ml-2 xl:ml-4 text-white bg-pink-500 border-0 py-1.5 md:py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded text-sm md:text-base">Add to Cart</button>
-                            <button className="rounded-full w-9 md:w-10 h-9 md:h-10 bg-pink-100 p-0 border-0 inline-flex items-center justify-center text-pink-500 ml-2 xl:ml-4">
+                            <button className="rounded-full w-9 md:w-10 h-9 md:h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-2 xl:ml-4">
                                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
                                 </svg>
