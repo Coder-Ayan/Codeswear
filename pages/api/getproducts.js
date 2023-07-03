d792ad7fe0c64cb0d2e31f5773b06706c957285e
Ayan Das Karmakar <coderayan1411@gmail.com>
2023-07-03 11:48:25 +0530

    Made changes in Get Products API & showed fetched tshirts

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -2,8 +2,15 @@ import Product from '../../models/Product'
 import connectToMongoDB from '../../middlewares/connectToMongoDB'
 
 const handler = async (req, res) => {
-    let products = await Product.find()
-    res.status(200).json({ products })
+    const category = req.query.category;
+    let products;
+
+    if (category) {
+        products = await Product.find({ category })
+    } else {
+        products = await Product.find()
+    }
+    res.status(200).json(products)
 }
 
 export default connectToMongoDB(handler)
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,118 +1,41 @@
 import Link from 'next/link'
 
-const tshirts = () => {
+const tshirts = ({ products }) => {
+    console.log(products)
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 my-16 mx-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
-                    <Link href="/product/wear-the-code">
-                        <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
-                            <div className="block relative rounded overflow-hidden">
-                                <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src="https://m.media-amazon.com/images/I/61K4wziiNhL._UX679_.jpg" />
-                            </div>
-                            <div className="mt-4 text-center">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the Code</h2>
-                                <p className="mt-1">&#8377;499</p>
-                                <p className="mt-1">S, M, L, XL, XXL</p>
-                            </div>
-                        </div>
-                    </Link>
+                    {
+                        products.map((product) => {
+                            return (
+                                <Link href={`/product/${product.slug}`}>
+                                    <div className="shadow-md shadow-gray-200 p-5 rounded-md border-[1px] border-gray-100">
+                                        <div className="block relative rounded overflow-hidden">
+                                            <img alt="ecommerce" className="m-auto h-[16rem] lg:h-[20rem] block" src={product.image} />
+                                        </div>
+                                        <div className="mt-4">
+                                            <h3 className="text-gray-500 text-xs tracking-widest uppercase mb-1">{product.category}</h3>
+                                            <h2 className="text-gray-900 title-font text-lg font-medium">{product.title}</h2>
+                                            <p className="mt-1 text-sm">{product.size} | {product.color}</p>
+                                            <p className="mt-1 text-gray-800 font-medium">&#8377;{product.price}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </section>
     )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch("http://localhost:3000/api/getproducts?category=tshirt")
+    const products = await response.json()
+
+    return { props: { products } }
 }
 
 export default tshirts
